ec6ce3626d4f43684eadf838554019fc4ad23138
Anna Lukianova <ann.lookyanova@gmail.com>
2022-11-17 22:03:42 +0100

    Create in memory service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 import { AppComponent } from './app.component';
 import { PinViewComponent } from './pin-view/pin-view.component';
 import { SelectOperationViewComponent } from './select-operation-view/select-operation-view.component';
@@ -17,7 +20,11 @@ import { WithdrawalViewComponent } from './withdrawal-view/withdrawal-view.compo
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
